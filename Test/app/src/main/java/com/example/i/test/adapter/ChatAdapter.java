b34c00c983b237963bf918382405ea672479dac5
unknown <>
2017-03-24 10:38:41 +0800

    three

---
+++ Test/app/src/main/java/com/example/i/test/adapter/ChatAdapter.java
@@ -0,0 +1,31 @@
+package com.example.i.test.adapter;
+
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+
+/**
+ * Created by 马仲凯 on 2017/3/24.
+ */
+
+public class ChatAdapter extends BaseAdapter {
+    @Override
+    public int getCount() {
+        return 0;
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        return null;
+    }
+}
--- Test/app/src/main/java/com/example/i/test/fragment/GroupFragment.java
+++ Test/app/src/main/java/com/example/i/test/fragment/GroupFragment.java
@@ -1,40 +1,26 @@
 package com.example.i.test.fragment;
 
-import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ListView;
-
-import com.example.i.test.R;
 
 /**
- * Created by 马仲凯 on 2017/3/23.
+ * Created by 马仲凯 on 2017/3/24.
  */
 
-public class ChatFragment extends Fragment{
-private ListView lv;
-    private Context context;
-   private View view;
+public class GroupFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-         super.onCreateView(inflater, container, savedInstanceState);
-
-         view=LayoutInflater.from(context).inflate(R.layout.fragmemt_chat,container,false);
-        return view;
+        return super.onCreateView(inflater, container, savedInstanceState);
     }
 
     @Override
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-
     }
-private void initView(){
-    lv= (ListView) view.findViewById(R.id.lv_char);
 
-}
 }
